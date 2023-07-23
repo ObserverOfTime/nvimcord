662249c1cbd89e01bbf811ca4418a5a193e0302e
ObserverOfTime <chronobserver@disroot.org>
2023-07-23 11:18:00 +0300

    Implement git workspace detection

--- lua/nvimcord/workspace.lua
+++ lua/nvimcord/workspace.lua
@@ -2,12 +2,25 @@ local M = {}
 
 ---@return string
 function M.get_url()
-    return ''
+    local o = vim.system({
+        'git', 'remote', 'get-url', 'origin'
+    }, {text = true}):wait()
+    if o.code ~= 0 then return '' end
+    local pattern = [[\%([a-z+]\+://\)\?\%(\w\+@\)\?\(.\{-1,}\)[:/]\(.\{-1,}\)\%(.git\)\?\n]]
+    return vim.fn.substitute(o.stdout, pattern, [[https://\1/\2]], '')
 end
 
 ---@return string
 function M.get_name()
-    return vim.fs.basename(vim.loop.cwd() or '')
+    local cwd = vim.loop.cwd()
+    local home = vim.loop.os_homedir()
+    local git = vim.fs.find('.git', {
+        type = 'directory',
+        stop = home,
+        upward = true
+    })[1]
+    if git then git = vim.fs.dirname(git) end
+    return assert(vim.fs.basename(git or cwd or ''))
 end
 
 return M
