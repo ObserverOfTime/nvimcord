684f8f3605e4f4f0afc94fad5080ac7850735a29
Cyuria <55673467+Cyuria@users.noreply.github.com>
2023-07-23 11:50:10 +1000

    add git repo finding to workspace root

--- lua/nvimcord/workspace.lua
+++ lua/nvimcord/workspace.lua
@@ -5,9 +5,20 @@ function M.get_url()
     return ''
 end
 
+--@return string
+local function find_git_repo_dir()
+    -- Ripped straight from :help vim.fs.parents()
+    for dir in vim.fs.parents(vim.loop.cwd()) do
+        if vim.fn.isdirectory(dir .. "/.git") == 1 then
+            return dir
+        end
+    end
+    return ''
+end
+
 ---@return string
 function M.get_name()
-    return vim.fs.basename(vim.loop.cwd() or '')
+    return vim.fs.basename(find_git_repo_dir() or vim.loop.cwd() or '')
 end
 
 return M
